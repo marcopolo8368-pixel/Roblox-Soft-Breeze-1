2f6f29ed3e04789645d1459cdf8a552c06db21c0
marcopolo8368-pixel <238919060+marcopolo8368-pixel@users.noreply.github.com>
2026-03-04 00:45:47 +0100

    Add LocationsConfig.lua with spawn locations

---
+++ ReplicatedStorage/LocationsConfig.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		name = "Spawn",
+		position = Vector3.new(0.047, 15.5, -7),
+	},
+	{
+		name = "Beach",
+		position = Vector3.new(222, 15.5, -188),
+	},
+	{
+		name = "Lamphouse",
+		position = Vector3.new(1311.877, 19.495, -676.232),
+	},
+}
